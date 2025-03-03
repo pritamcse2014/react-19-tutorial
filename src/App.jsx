78b23a6123229d704a2e2b747a556080b7bc5148
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2025-03-03 19:14:57 +0600

    importing-and-exporting-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
 import React from 'react';
+import User, { Profile, Setting, UserKey } from './User';
 
 const App = () => {
   return (
     <div>
-      <h2>First Component</h2>
-      <Fruit/>
-      <Colors/>
-      <Sum/>
+      <h2>Importing and Exporting Components</h2>
+      <User/>
+      <Profile/>
+      <Setting/>
+      <h2>{UserKey}</h2>
     </div>
-  );
-};
-
-const Fruit = () => {
-  return (
-    <div>
-      <h2>Apple</h2>
-    </div>
-  );
-};
-
-const Colors = () => {
-  return (
-    <div>
-      <h2>Red</h2>
-    </div>
-  );
-};
-
-const Sum = () => {
-  return (
-    15 + 10
   );
 };
 
---
+++ src/User.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div>
+            <h2>Login User</h2>
+        </div>
+    );
+};
+
+export default User;
+
+export function Profile() {
+    return (
+        <div>
+            <h2>Profile</h2>
+        </div>
+    )
+}
+
+export function Setting() {
+    return (
+        <div>
+            <h2>Setting</h2>
+        </div>
+    )
+}
+
+export const UserKey = "!@#$%^&*(){}:;'/?.<>"
